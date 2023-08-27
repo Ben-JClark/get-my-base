0107370622a4618ee55adb7aab9f186b67d39316
Ben-JClark <benclarkat@proton.me>
2023-08-28 07:26:23 +1200

    added bootstrap cards

--- only-bases/src/components/BaseCard.tsx
+++ only-bases/src/components/BaseCard.tsx
@@ -1,3 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import mainImage from "../assets/main-image.jpg";
+import smallImageUp from "../assets/seconday-image-up.jpg";
+import smallImageDown from "../assets/seconday-image-down.jpg";
+
 interface Props {
   id: string;
   title: string;
@@ -11,12 +17,35 @@ interface Props {
  */
 function BaseCard({ id, title, creator, rockets, exploits }: Props) {
   return (
-    <>
-      <p>
-        id:{id} title:{title} creator:{creator} rockets:{rockets} exploits:
-        {exploits == true ? "Expoints" : "No Exploits"}
-      </p>
-    </>
+    <div className="card">
+      <div className="card-header">{title}</div>
+      <div className="card-body">
+        <div className="row">
+          <div className="col-lg-8">
+            <img src={mainImage} alt="Large Image" className="img-fluid" />
+          </div>
+          <div className="col-lg-4">
+            <div className="row">
+              <div className="col-sm-12 mb-3">
+                <img
+                  src={smallImageUp}
+                  alt="Small Image 1"
+                  className="img-fluid"
+                />
+              </div>
+              <div className="col-sm-12">
+                <img
+                  src={smallImageDown}
+                  alt="Small Image 2"
+                  className="img-fluid"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-footer">Card Footer</div>
+    </div>
   );
 }
 
